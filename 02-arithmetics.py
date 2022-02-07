7199cfdfa11ea716889ca8b10f77d4f7ef6f364b
Amrit Koirala <grepamrit@gmail.com>
2022-02-07 21:28:53 +0545

    Added 02-arithmetic.py file

---
+++ 02-arithmetics.py
@@ -0,0 +1,10 @@
+import math 
+
+# Modulo 
+number = 39
+limit = 20
+print(number%limit) # % is a Mudulo Operator
+
+# Power Operator
+print (3**3) # 3 power of 3 
+print(math.pow(3,3)) # using math module for powering 
