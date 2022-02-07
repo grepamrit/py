d871164925a51d5442cd16fe123f3391fcf637a4
Amrit Koirala <amrit@moco.com.np>
2022-02-07 18:54:55 +0545

    Finishing up math operation & pushing 001_Math.py

--- 001_Math.py
+++ 001_Math.py
@@ -1,9 +1,24 @@
-
-# This is comments
-
+# using math modules
 import math 
 
-result = 10/3
-print(result)
+# Normal Operation 
+print("\nThis is a Normal Operation:")
+print(10/3)  # Float Division 
+print(10//3) # Integer Division 
+print("\n")
 
-print (round(result))
+# using floor 
+print("This is a floor value:")
+print(math.floor(10/3))
+print("\n")
+
+# using ceil 
+print("This is a ceil value:")
+print(math.ceil(10/3))
+print("\n")
+
+# using round
+print("Using round Operations: ")
+print(round(10/3))  
+print(round(10/3,4)) # this prints out the 4 digit after decimal
+print(round(20/3,2))
